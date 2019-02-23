50cc94c4a71ed9c0bde089df838ad3dd159dddc3
anthonymorris1 <39576798+anthonymorris1@users.noreply.github.com>
2019-02-23 17:06:46 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <CoffeeCalculator msg="Ayooooo river"/>
+    
+  </div>
+</template>
+
+<script>
+import CoffeeCalculator from './components/CoffeeCalculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CoffeeCalculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color:black;
+  margin-top: 60px;
+}
+</style>
