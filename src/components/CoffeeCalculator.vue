e89b02ddbdb6e46442ea87530e0cb7cc3eed5dea
anthonymorris1 <39576798+anthonymorris1@users.noreply.github.com>
2019-02-23 19:27:17 -0600

    implements basic calculation

--- src/components/CoffeeCalculator.vue
+++ src/components/CoffeeCalculator.vue
@@ -1,40 +1,41 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1> Coffee Calculator </h1>
+    <h2> Brew Method </h2>
+      <select v-model="brew_method"> <!--binds brew_method to data in javascript-->
+        <option value="volvo">French Press</option>
+        <option value="saab">Aeropress</option>
+        <option value="mercedes">Drip</option>
+        <option value="audi">Pour Over</option>
+      </select>
+    <br>
+    Ground coffee (grams): <input type="text" v-model="ground_coffee" ><br>
+    Water (grams): <input type="text" v-model="water" ><br>
+    <h3>selected brew method: {{ brew_method }}</h3> 
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'CoffeeCalculator',
-  props: {
-    msg: String
+  props: {},
+  data() {
+    return {
+    brew_method: 'audi',
+    ground_coffee: '0',
+    water: '0'
+    }
+  },
+  watch: {
+    ground_coffee(val,old_val) {
+      console.log('calculate event',val,old_val)
+      this.$set(this, 'water', val*16)
+    },
+      water(val,old_val) {
+      console.log('calculate event',val,old_val)
+      this.$set(this, 'ground_coffee', val*(1/16))
+    }
   }
 }
 </script>
